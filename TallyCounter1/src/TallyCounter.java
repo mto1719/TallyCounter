d4db02ed1c147400119dc39b19d0235430e01763
mto1719 <42977020+mto1719@users.noreply.github.com>
2018-09-26 12:54:30 -0400

    Add files via upload

--- TallyCounter1/src/TallyCounter.java
+++ TallyCounter1/src/TallyCounter.java
@@ -27,11 +27,20 @@ public class TallyCounter
 	
 	public void click()
 	{
-		count = a + count + 1;//count++ = count + 1
+		count = count + 1;//count++ = count + 1
 		//another option is count += 1
 	}
 
-	public void getCount();
-	public void reset();
-	public void unClick();
+	public int getCount()
+	{
+		return count;
+	}
+	public void reset()
+	{
+		count = 0;
+	}
+	public void unClick()
+	{
+		count--;
+	}
 }
